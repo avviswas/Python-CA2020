2e74b9aa1ba01a18f45999aa1d9940685e84d6c1
avviswas <alvinviswas@gmail.com>
2020-12-20 20:33:17 -0500

    Attaching screenshot

--- Exercise-6/code.py
+++ Exercise-6/code.py
@@ -1,6 +1,7 @@
 #function to add two numbers
-def add(a.b):
+def(a.b):
   return a+b
+<<<<<<< HEAD
 
 #function to subtract two numbers
 def sub(a,b):
@@ -16,3 +17,5 @@ def div(a,b)
   return(a/b)
 =======
 >>>>>>> parent of bcf8e83... commit 3
+=======
+>>>>>>> parent of 64e6e80... commit 2
